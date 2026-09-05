c312b848d63eb17474ca819a2be319910b86b73e
Mohammed Suhaib <raksssuhaib@gmail.com>
2026-09-05 05:51:16 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = "calculator-pro-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+
+/* INSTALL */
+self.addEventListener("install", event => {
+  
+  event.waitUntil(
+    
+    caches.open(CACHE_NAME).then(cache => {
+      
+      return cache.addAll(FILES);
+      
+    })
+    
+  );
+  
+  self.skipWaiting();
+  
+});
+
+
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  
+  event.waitUntil(
+    
+    caches.keys().then(cacheNames => {
+      
+      return Promise.all(
+        
+        cacheNames
+        .filter(name => name !== CACHE_NAME)
+        .map(name => caches.delete(name))
+        
+      );
+      
+    })
+    
+  );
+  
+  self.clients.claim();
+  
+});
+
+
+/* FETCH */
+self.addEventListener("fetch", event => {
+  
+  event.respondWith(
+    
+    caches.match(event.request).then(cachedResponse => {
+      
+      if (cachedResponse) {
+        
+        return cachedResponse;
+        
+      }
+      
+      return fetch(event.request);
+      
+    })
+    
+  );
+  
+});
